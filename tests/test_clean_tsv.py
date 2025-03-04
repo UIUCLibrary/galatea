77c0ff7bcc8d748dbba797caed58105e09307e2f
Henry Borchers <henryborchers@yahoo.com>
2025-03-04 11:56:20 -0600

    feat: clean-tsv prints diff when --verbose flag is used

--- tests/test_clean_tsv.py
+++ tests/test_clean_tsv.py
@@ -100,7 +100,7 @@ def test_iter_tsv_fp(sample_tsv_file_pointer):
             clean_tsv.iter_tsv_fp(
                 sample_tsv_file_pointer, dialect=csv.get_dialect("excel-tab")
             )
-        )["1"]
+        ).entry["1"]
         == "99160807512205899"
     )
 
@@ -160,7 +160,7 @@ def test_remembered_file_pointer_head(sample_tsv_file_pointer):
 def test_clean_tsv(monkeypatch):
     write_tsv_file = Mock(name="write_tsv_file")
     monkeypatch.setattr(clean_tsv, "write_tsv_file", write_tsv_file)
-
+    monkeypatch.setattr(clean_tsv, "get_field_names", Mock(return_value=[]))
     with patch("galatea.clean_tsv.open", mock_open(read_data="")):
         clean_tsv.clean_tsv(
             pathlib.Path("source.tsv"), pathlib.Path("output.tsv")
@@ -226,6 +226,7 @@ def test_transform_row_and_merge(marc_entry):
         merged_row["246"] == "dummy" and merged_row["1"] == "99160807512205899"
     )
 
+
 class TestRowTransformer:
     def test_no_transform(self, marc_entry):
         transformer = clean_tsv.RowTransformer()
@@ -236,27 +237,103 @@ class TestRowTransformer:
 
         transformer.add_transformation(modifiers.remove_trailing_periods)
         transformed = transformer.transform(marc_entry)
-        assert all([
-            # the original value ends with a period
-            marc_entry["245"][-1] == ".",
-            marc_entry["110"][-1] == ".",
-            # the transformed value does not end with a period
-            transformed["245"][-1] != ".",
-            transformed["110"][-1] != ".",
-        ]), f"""original["245"] = {marc_entry["245"]}
-                transformed['245'] = {transformed['245']}"""
+        assert all(
+            [
+                # the original value ends with a period
+                marc_entry["245"][-1] == ".",
+                marc_entry["110"][-1] == ".",
+                # the transformed value does not end with a period
+                transformed["245"][-1] != ".",
+                transformed["110"][-1] != ".",
+            ]
+        ), f"""original["245"] = {marc_entry["245"]}
+                transformed['245'] = {transformed["245"]}"""
 
     def test_add_transformation_to_specific_field(self, marc_entry):
         transformer = clean_tsv.RowTransformer()
         transformer.add_transformation(
             modifiers.remove_trailing_periods,
-            condition=lambda key,_: key == "245"
+            condition=lambda key, _: key == "245",
         )
-        assert all([
-            # the original value ends with a period
-            marc_entry["245"][-1] == ".",
-            marc_entry["110"][-1] == ".",
-            # only the entry with 245 key transformed value to remove a period
-            transformer.transform(marc_entry)["245"][-1] != ".",
-            transformer.transform(marc_entry)["110"][-1] == ".",
-            ])
+        assert all(
+            [
+                # the original value ends with a period
+                marc_entry["245"][-1] == ".",
+                marc_entry["110"][-1] == ".",
+                # only the entry with 245 key transformed value to remove a period
+                transformer.transform(marc_entry)["245"][-1] != ".",
+                transformer.transform(marc_entry)["110"][-1] == ".",
+            ]
+        )
+
+
+def test_get_field_names_calls_fp_strategy():
+    strategy = Mock()
+    with patch("galatea.clean_tsv.open", mock_open()):
+        clean_tsv.get_field_names("somepath", "excel-tab", strategy)
+    strategy.assert_called_once()
+
+
+def test_get_field_names_fp():
+    with io.StringIO() as buff:
+        buff.write(
+            "1	50	100	110	111	245	246	255	260$a"
+        )
+        buff.seek(0)
+        assert clean_tsv.get_field_names_fp(buff, dialect="excel-tab") == [
+            "1",
+            "50",
+            "100",
+            "110",
+            "111",
+            "245",
+            "246",
+            "255",
+            "260$a",
+        ]
+
+
+def test_get_field_names_fp_raise_if_no_field_names_found():
+    with pytest.raises(ValueError):
+        with io.StringIO() as buff:
+            clean_tsv.get_field_names_fp(buff, dialect="excel-tab")
+
+
+def test_create_diff_report(marc_entry):
+    row_a = clean_tsv.TableRow(line_number=1, entry=marc_entry)
+    modified_data = marc_entry.copy()
+    modified_data["260$b"] = "The Division"
+    row_b = clean_tsv.TableRow(line_number=1, entry=modified_data)
+
+    assert "260$b" in clean_tsv.create_diff_report(
+        row_a, row_b, marc_entry.keys()
+    )
+
+
+def test_create_diff_report_no_changes(marc_entry):
+    row_a = clean_tsv.TableRow(line_number=1, entry=marc_entry)
+    row_b = clean_tsv.TableRow(line_number=1, entry=marc_entry)
+
+    assert (
+        clean_tsv.create_diff_report(row_a, row_b, marc_entry.keys()) is None
+    )
+
+
+def test_clean_tsv_row_diff_report_generator_called(monkeypatch, marc_entry):
+    write_tsv_file = Mock(name="write_tsv_file")
+    monkeypatch.setattr(clean_tsv, "write_tsv_file", write_tsv_file)
+    monkeypatch.setattr(
+        clean_tsv, "get_field_names", Mock(return_value=marc_entry.keys())
+    )
+    row_one = clean_tsv.TableRow(1, marc_entry)
+    monkeypatch.setattr(clean_tsv, "iter_tsv_fp", Mock(return_value=[row_one]))
+    row_diff_report_generator = Mock()
+    with patch("galatea.clean_tsv.open", mock_open(read_data="")):
+        clean_tsv.clean_tsv(
+            pathlib.Path("source.tsv"),
+            pathlib.Path("output.tsv"),
+            row_diff_report_generator=row_diff_report_generator,
+        )
+    row_diff_report_generator.assert_called_once_with(
+        row_one, ANY, marc_entry.keys()
+    )
