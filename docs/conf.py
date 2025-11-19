e69fe41115a0658876d44830abb9732b74bd1dd9
Henry Borchers <henryborchers@yahoo.com>
2025-11-19 08:48:28 -0600

    docs: Wider columns

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 html_static_path = ['_static']
 html_logo = '_static/full_mark_horz_bw.gif'
-
+html_theme_options = {
+    'body_max_width' : '90%',
+    'page_width': '70%'
+}
 linkcheck_ignore = [
     "https://example.com/"
 ]
